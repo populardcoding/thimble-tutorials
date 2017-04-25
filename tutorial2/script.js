db5ee818084e4cfc3090dbcf07bc66c8fbc206a8
Jose Marquez <jimarqz@gmail.com>
2017-04-24 21:36:37 -0400

    new sample

---
+++ tutorial2/script.js
@@ -0,0 +1,25 @@
+/***************************
+This is JavaScript (JS), the programming language that powers the web (and this is a comment, which you can delete).
+
+To use this file, link it to your markup by placing a <script> in the <body> of your HTML file:
+
+  <body>
+    <script src="script.js"></script>
+
+replacing "script.js" with the name of this JS file.
+
+Learn more about JavaScript at
+
+https://developer.mozilla.org/en-US/Learn/JavaScript
+***************************/
+
+function hello() {
+  var input = document.getElementById("userInput").value;
+  alert("Hello" + " " + input + ", welcome to D-Coding");
+}
+
+
+function chBackcolor(color) {
+  document.body.style.background = color;
+
+}
